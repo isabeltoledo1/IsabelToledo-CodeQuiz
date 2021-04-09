a43c6dedbc24a3866d5186ae487ab90046710528
Isabel Toledo <isabeltoledo9@gmail.com>
2021-04-08 23:18:33 -0600

    added questions

--- gameQuestions.js
+++ gameQuestions.js
@@ -1,32 +1,66 @@
-var questions = [
+var myQuestions = [
 {
-   title:
-   choices: 
-   answer:
+   question: "JavaScript is interpreted by _________",
+   choices: [
+       "A: Client",
+       "B: Server",
+       "C: Object"
+   ],
+   answer: "A"
 },
 
 
 {
-    title:
-    choices: 
-    answer:
+    question: "The _______ method of an Array object adds and/or removes elements from an array.",
+    choices: [
+        "A: Reverse",
+        "B: Delete",
+        "C: Splice"
+    ],
+    answer: "C"
  },
  
 
 {
-    title:
-    choices: 
-    answer:
+    question: "Which of the following are capabilities of functions in JavaScript?"
+    choices: [
+        "A: Return a value",
+        "B: Accept parameters and Return a value",
+        "C: Accept parameters"
+    ],
+    answer: "C"
  },
  
 
-{
-    
-    title:
-    choices: 
-    answer:
+{  
+    question: "	If we want to wrap a block of text around an image, which css property will we use ?"
+    choices: [
+        "A: Wrap",
+        "B: Float",
+        "C: Align"
+    ],
+    answer: "B"
  },
  
-
+ {  
+    question: "The default value of 'position' attribute is _________."
+    choices: [
+        "A: Relative",
+        "B: Absolute",
+        "C: Fixed"
+    ],
+    answer: "A"
+ },
+ 
+ {  
+    question: "Original Name of Javascript is"
+    choices: [
+        "A: Mocha",
+        "B: LiveScript",
+        "C: Escript"
+    ],
+    answer: "B"
+ },
+ 
 
 ];
