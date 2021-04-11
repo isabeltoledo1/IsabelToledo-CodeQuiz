efaaa4472d17ed6219a97928a4bb4618b6ecaa73
Isabel Toledo <isabeltoledo9@gmail.com>
2021-04-11 12:03:04 -0600

    added scores and question counter to game

--- scores.js
+++ scores.js
@@ -1,3 +1,18 @@
   //end screen
-  var endScreenEl = document.getElementById("highscores");
-  endScreenEl.removeAttribute("class");
+
+  //var endScreenEl = document.getElementById("end-screen");
+  //endScreenEl.removeAttribute("class");
+
+  //var endScreenEl = document.getElementById("highscores");
+ // endScreenEl.removeAttribute("class");
+
+  const highScoresList = document.getElementById("highScoresList");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+highScoresList.innerHTML = highScores
+    .map(score => {
+        return `<li class="high-score">${score.name} - ${score.score}</li>`;
+
+    })
+
+    .join("");
